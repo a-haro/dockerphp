3714a25c3f6f15c5f61e4e567f85787f83a9d889
aharo <antonioharo655@gmail.com>
2023-11-09 17:20:10 +0100

    exercicis bidimensionals

--- volums/web/exercicisArrays.php
+++ volums/web/exercicisArrays.php
@@ -1,161 +1,187 @@
 <!DOCTYPE html>
 <html>
+
 <body>
 
-<h1>Exercicis arrays PHP bidimensionales</h1>
+    <h1>Exercicis arrays PHP</h1>
 
-<b>Ejercicio 1</b>
-<br><br>
-<?php
-$arrayAleatori = array();
-$arrLong = 10;
-for ($i = 0; $i < $arrLong; $i++){
-    $numAleatori = rand(1,20);
-    $arrayAleatori[$i] = $numAleatori;
-}
-$arrLong = count($arrayAleatori);
-for ($i = 0; $i < $arrLong; $i++){
-    print $arrayAleatori[$i];
-    print "<br>";
-}
-?>
-<br><br>
-<b>Ejercicio 2</b>
-<br><br>
-<?php
-$maxValor = max($arrayAleatori);
-$minValor = min($arrayAleatori);
-print("El valor mínimo es $minValor y el máximo es $maxValor");
-?>
-<br><br>
-<b>Ejercicio 3</b>
-<br><br>
-<?php
-$longArr = count($arrayAleatori);
-print("El array tiene $longArr elementos");
-?>
-<br><br>
-<b>Ejercicio 4</b>
-<br><br>
-<?php
-$sumaArray = array_sum($arrayAleatori);
-$mediaArray = $sumaArray/$longArr;
-print("La suma de los elementos es $sumaArray y la media $mediaArray");
-?>
-<br><br>
-<b>Ejercicio 5</b>
-<br><br>
-<?php
-print(implode(",",$arrayAleatori));
-?>
-<br><br>
-<b>Ejercicio 6</b>
-<br><br>
-<?php
-$arrNombres = array("Alfredo","Manuel","Lolito","Ambrosio","Zapatero","Manel","Toni","Joan","Bartolomeu");
-sort($arrNombres);
-$arrLong = count($arrNombres);
-print("Lista ascendente: <br><br>");
-for ($i = 0; $i < $arrLong; $i++){
-    print $arrNombres[$i];
-    print "<br>";
-}
-print ("<br>");
-print("Lista descendente: <br><br>");
-rsort($arrNombres);
-for ($i = 0; $i < $arrLong; $i++){
-    print $arrNombres[$i];
-    print "<br>";
-}
-?>
-<br><br>
-<b>Ejercicio 7</b>
-<br><br>
-<table border=1>
-<?php
-sort($arrNombres);
-for ($i = 0; $i < $arrLong; $i++){
-    print "<tr>
+    <b>Ejercicio 1</b>
+    <br><br>
+    <?php
+    $arrayAleatori = array();
+    $arrLong = 10;
+    for ($i = 0; $i < $arrLong; $i++) {
+        $numAleatori = rand(1, 20);
+        $arrayAleatori[$i] = $numAleatori;
+    }
+    $arrLong = count($arrayAleatori);
+    for ($i = 0; $i < $arrLong; $i++) {
+        print $arrayAleatori[$i];
+        print "<br>";
+    }
+    ?>
+    <br><br>
+    <b>Ejercicio 2</b>
+    <br><br>
+    <?php
+    $maxValor = max($arrayAleatori);
+    $minValor = min($arrayAleatori);
+    print("El valor mínimo es $minValor y el máximo es $maxValor");
+    ?>
+    <br><br>
+    <b>Ejercicio 3</b>
+    <br><br>
+    <?php
+    $longArr = count($arrayAleatori);
+    print("El array tiene $longArr elementos");
+    ?>
+    <br><br>
+    <b>Ejercicio 4</b>
+    <br><br>
+    <?php
+    $sumaArray = array_sum($arrayAleatori);
+    $mediaArray = $sumaArray / $longArr;
+    print("La suma de los elementos es $sumaArray y la media $mediaArray");
+    ?>
+    <br><br>
+    <b>Ejercicio 5</b>
+    <br><br>
+    <?php
+    print(implode(",", $arrayAleatori));
+    ?>
+    <br><br>
+    <b>Ejercicio 6</b>
+    <br><br>
+    <?php
+    $arrNombres = array("Alfredo", "Manuel", "Lolito", "Ambrosio", "Zapatero", "Manel", "Toni", "Joan", "Bartolomeu");
+    sort($arrNombres);
+    $arrLong = count($arrNombres);
+    print("Lista ascendente: <br><br>");
+    for ($i = 0; $i < $arrLong; $i++) {
+        print $arrNombres[$i];
+        print "<br>";
+    }
+    print("<br>");
+    print("Lista descendente: <br><br>");
+    rsort($arrNombres);
+    for ($i = 0; $i < $arrLong; $i++) {
+        print $arrNombres[$i];
+        print "<br>";
+    }
+    ?>
+    <br><br>
+    <b>Ejercicio 7</b>
+    <br><br>
+    <table border=1>
+        <?php
+        sort($arrNombres);
+        for ($i = 0; $i < $arrLong; $i++) {
+            print "<tr>
                 <td>$arrNombres[$i]</td>
            </tr>";
-}
-?>
-</table>
-<br><br>
-<b>Ejercicio 8</b>
-<br><br>
-<?php
-$elementoDonat = 3;
-if (in_array($elementoDonat, $arrayAleatori)){
-    print "$elementoDonat está en el array";
-} else {
-    print "$elementoDonat no se encuentra en el array";
-}
-?>
-<br><br>
-<b>Ejercicio 9</b>
-<br><br>
-<?php
-$arrayRepetida = array(1,3,5,3,5,2,6,7,8,6,7);
-print "<br>Array con números repetidos:<br>";
-$arrLong = count($arrayRepetida);
-for ($i = 0; $i < $arrLong; $i++){
-    print $arrayRepetida[$i];
-    print "<br>";
-}
-$arraySinRepetir = array_unique($arrayRepetida);
-print "<br>Array sin repetir:<br>";
-$arrLong = count($arrayRepetida);
-for ($i = 0; $i < $arrLong; $i++){
-    print $arraySinRepetir[$i];
-    print "<br>";
-}
-?>
-<br><br>
-<b>Ejercicio 10</b>
-<br><br>
-<?php
-$arrayNumsVaris = array(1,-3,0,5,6,-2,0,-1,8);
-$numsPositivos = 0;
-$numsZeros = 0;
-$numsNegativos = 0;
-$arrLong = count($arrayNumsVaris);
-for ($i = 0; $i < $arrLong; $i++){
-    if ($arrayNumsVaris[$i] > 0){
-        $numsPositivos++;
-    } elseif ($arrayNumsVaris[$i] == 0){
-        $numsZeros++;
-    }else {
-        $numsNegativos++;
+        }
+        ?>
+    </table>
+    <br><br>
+    <b>Ejercicio 8</b>
+    <br><br>
+    <?php
+    $elementoDonat = 3;
+    if (in_array($elementoDonat, $arrayAleatori)) {
+        print "$elementoDonat está en el array";
+    } else {
+        print "$elementoDonat no se encuentra en el array";
     }
-}
-print "Array: <br>";
-for ($i = 0; $i < $arrLong; $i++){
-    print $arrayNumsVaris[$i];
+    ?>
+    <br><br>
+    <b>Ejercicio 9</b>
+    <br><br>
+    <?php
+    $arrayRepetida = array(1, 3, 5, 3, 5, 2, 6, 7, 8, 6, 7);
+    print "<br>Array con números repetidos:<br>";
+    $arrLong = count($arrayRepetida);
+    for ($i = 0; $i < $arrLong; $i++) {
+        print $arrayRepetida[$i];
+        print "<br>";
+    }
+    $arraySinRepetir = array_unique($arrayRepetida);
+    print "<br>Array sin repetir:<br>";
+    $arrLong = count($arrayRepetida);
+    for ($i = 0; $i < $arrLong; $i++) {
+        print $arraySinRepetir[$i];
+        print "<br>";
+    }
+    ?>
+    <br><br>
+    <b>Ejercicio 10</b>
+    <br><br>
+    <?php
+    $arrayNumsVaris = array(1, -3, 0, 5, 6, -2, 0, -1, 8);
+    $numsPositivos = 0;
+    $numsZeros = 0;
+    $numsNegativos = 0;
+    $arrLong = count($arrayNumsVaris);
+    for ($i = 0; $i < $arrLong; $i++) {
+        if ($arrayNumsVaris[$i] > 0) {
+            $numsPositivos++;
+        } elseif ($arrayNumsVaris[$i] == 0) {
+            $numsZeros++;
+        } else {
+            $numsNegativos++;
+        }
+    }
+    print "Array: <br>";
+    for ($i = 0; $i < $arrLong; $i++) {
+        print $arrayNumsVaris[$i];
+        print "<br>";
+    }
+    print "Hay $numsPositivos números positivos<br>";
+    print "Hay $numsNegativos números negativos<br>";
+    print "Hay $numsZeros ceros<br>";
+    ?>
+    <br><br>
+    <b>Ejercicio 11</b>
+    <br><br>
+    <?php
+    $arraySorted = array(1, 2, 3, 4);
+    $arrLong = count($arraySorted);
+    for ($i = 0; $i < $arrLong; $i++) {
+        print $arraySorted[$i];
+        print "<br>";
+    }
+    print "Vamos a introducir 2 números, 7 y 8<br>";
+    $arraySorted[] = 7;
+    $arraySorted[] = 8;
+    $arrLong = count($arraySorted);
+    for ($i = 0; $i < $arrLong; $i++) {
+        print $arraySorted[$i];
+        print "<br>";
+    }
+    ?>
+    <br><br>
+    <b>Ejercicio 12</b>
+    <br><br>
+    <?php
+    array_pop($arraySorted);
+    array_pop($arraySorted);
+    $arrLong = count($arraySorted);
+    for ($i = 0; $i < $arrLong; $i++) {
+        print $arraySorted[$i];
+        print "<br>";
+    }
+    ?>
+    <br><br>
+    <b>Ejercicio 13</b>
+    <br><br>
+    <?php
+    $arrayIFC31A = array("Antonio", "Pere", "Paco", "Jaume", "Alex", "Ruben");
+    $arrayIFC31B = array("Tarik", "Pere", "Ana", "Toni", "Pedro", "Juan");
+    print_r (array_diff($arrayIFC31A, $arrayIFC31B));
     print "<br>";
-}
-print "Hay $numsPositivos números positivos<br>";
-print "Hay $numsNegativos números negativos<br>";
-print "Hay $numsZeros ceros<br>";
-?>
-<br><br>
-<b>Ejercicio 11</b>
-<br><br>
-<?php
-$arraySorted = array(1,2,3,4);
-$arrLong = count($arraySorted);
-for ($i = 0; $i < $arrLong; $i++){
-    print $arraySorted[$i];
+    print_r (array_intersect($arrayIFC31A, $arrayIFC31B));
     print "<br>";
-}
-print "Vamos a introducir 2 números, 7 y 8<br>";
-$arraySorted[] = 7;
-$arraySorted[] = 8;
-$arrLong = count($arraySorted);
-for ($i = 0; $i < $arrLong; $i++){
-    print $arraySorted[$i];
-    print "<br>";
-}
-?>
+    print_r (array_merge($arrayIFC31A, $arrayIFC31B));
+    ?>
 </body>
+
 </html>
---
+++ volums/web/exercicisArraysBidimensionals.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h1>Exercicis arrays PHP multidimensional</h1>
+    <b>Ejercicio 1</b>
+    <br><br>
+    <?php
+    $matriu = array(
+        array(2, 5, 3),
+        array(4, 6, 7),
+        array(5, 7, 8)
+    );
+    $total = 0;
+    $elements = 0;
+    for ($i = 0; $i < count($matriu[0]); $i++) {
+        $columna_actual = array_column($matriu, $i);
+        $suma_actual = array_sum($columna_actual);
+        $total += $suma_actual;
+        $elements += count($columna_actual);
+    }
+    print "La suma de los elementos es $total <br>";
+    print "Hay $elements elementos";
+    ?>
+    <br><br>
+    <b>Ejercicio 2</b>
+    <br><br>
+    <?php
+    $matriu = array(
+        array(2, 5, 3),
+        array(4, 6, 7),
+        array(5, 7, 8)
+    );
+    $total = 0;
+    $elements = 0;
+    for ($i = 0; $i < count($matriu[0]); $i++) {
+        $columna_actual = array_column($matriu, $i);
+        $suma_actual = array_sum($columna_actual);
+        $total += $suma_actual;
+        $elements += count($columna_actual);
+    }
+    print "La suma de los elementos es $total <br>";
+    print "Hay $elements elementos";
+    ?>
+    <br><br>
+    <b>Ejercicio 3</b>
+    <br><br>
+    <?php
+    $a = array(
+        array(1, 5, 8),
+        array(4, 3, 7),
+        array(5, 7, 6)
+    );
+
+    print max(array_map("max", $a));
+    ?>
+    <br><br>
+    <b>Ejercicio 4</b>
+    <br><br>
+    <?php
+    $count = 0;
+    foreach ($a as $fila) {
+        if (in_array(2, $fila)) {
+            print "Hay un 2";
+            $count++;
+        }
+    }
+    if ($count == 0) {
+        print "No hay un 2";
+    }
+    ?>
+    <br><br>
+    <b>Ejercicio 5</b>
+    <br><br>
+    <?php
+    $a = array(
+        array(1, 5, 8),
+        array(4, 3, 7),
+        array(5, 7, 6)
+    );
+    $valor_a_buscar = 5;
+
+    foreach ($a as $clave => $subarray) {
+        $clave_interna = array_search($valor_a_buscar, $subarray);
+
+        if ($clave_interna != false) {
+            echo "El valor $valor_a_buscar está en la fila: $clave de la columna: $clave_interna";
+            break;
+        }
+    }
+
+    if ($clave_interna === false) {
+        echo "El valor $valor_a_buscar no está en la matriz bidimensional.";
+    }
+
+    ?>
+</body>
+
+</html>
